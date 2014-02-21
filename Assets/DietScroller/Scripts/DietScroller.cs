df2b4d9d47a358a1f866c8a8a3956bf27d1c36cf
david90 <david.ng@oursky.com>
2014-02-21 16:53:59 +0800

    Enable horizontal / vertical scroll mode

--- Assets/DietScroller/Scripts/DietScroller.cs
+++ Assets/DietScroller/Scripts/DietScroller.cs
@@ -7,16 +7,34 @@ public class DietScroller : MonoBehaviour {
     public int bundle = 3;
     public GameObject itemPrefab;
     public float posX = 0f;
+	public float posY = 0f;
     public float cellWidth = 102;
     public float cellHeight = 102;
+
+	public enum ScrollMode { Horizontal, Vertical };
+	public ScrollMode scrollMode;
 
     GameObject[] itemList;
 
     void Start()
     {
+		// Update panel's scale
+		UIDraggablePanel draggablePanel = panel.GetComponent<UIDraggablePanel> ();
+
         BoxCollider col = collider as BoxCollider;
-        col.size = new Vector3(cellWidth * total, cellHeight, 1f);
-        col.center = new Vector3(cellWidth * (total - 1) / 2f, 0f, 0f);
+
+		if (scrollMode == ScrollMode.Horizontal)
+		{
+			draggablePanel.scale = new Vector3(1f, 0f, 1f); // allow horizontal scroll
+			col.size = new Vector3 (cellWidth * total, cellHeight, 1f);
+			col.center = new Vector3 (cellWidth * (total - 1) / 2f, 0f, 0f);
+		} else if (scrollMode == ScrollMode.Vertical)
+		{
+			draggablePanel.scale = new Vector3(0f, 1f, 1f); // allow vertical scroll
+			col.size = new Vector3 (cellWidth, cellHeight * total, 1f);
+			col.center = new Vector3 (0f, - cellHeight * (total - 1) / 2f, 0f);
+		}
+
         itemList = new GameObject[total];
         AddItem(0);
         AddItem(total - 1);
@@ -26,27 +44,57 @@ public class DietScroller : MonoBehaviour {
     {
         if (seq < 0 || seq >= total) return;
         if (itemList[seq]) return;
+
         GameObject go = NGUITools.AddChild(gameObject, itemPrefab);
-        go.transform.localPosition = Vector3.right * seq * cellWidth;
-        UILabel label = go.GetComponentInChildren<UILabel>();
-        string str = (seq + 1).ToString("0000");
-        label.text = str;
-        go.name = "item_" + str;
-        Item item = go.GetComponent<Item>();
-        item.min = -cellWidth;
-        item.max = (bundle) * cellWidth;
-        if (seq == 0 || seq == total - 1) item.on = true;
+		UpdateItem (go, seq);
+
+		// Save in list
         itemList[seq] = go;
     }
 
+	void UpdateItem(GameObject go, int seq)
+	{
+		// Update Content
+		UILabel label = go.GetComponentInChildren<UILabel>();
+		string str = (seq + 1).ToString("0000");
+		label.text = str;
+		go.name = "item_" + str;
+
+		// Update Position
+		Item item = go.GetComponent<Item> ();
+		if (scrollMode == ScrollMode.Horizontal)
+		{
+			go.transform.localPosition = Vector3.right * seq * cellWidth;
+			item.min = -cellWidth;
+			item.max = (bundle) * cellWidth;
+		} else if (scrollMode == ScrollMode.Vertical)
+		{
+			go.transform.localPosition = Vector3.down * seq * cellHeight;
+			item.min = -cellHeight;
+			item.max = (bundle) * cellHeight;
+		}
+
+		if (seq == 0 || seq == total - 1) item.on = true;
+	}
+
     void Update()
     {
-        posX = panel.localPosition.x;
-        int pos = Mathf.Abs(Mathf.FloorToInt(posX / cellWidth));
-        for (int i = -1; i < bundle; i++)
-        {
-            int seq = i + pos;
-            AddItem(i + pos);
-        }
+		int pos = 0;
+		if (scrollMode == ScrollMode.Horizontal) 
+		{
+			posX = panel.localPosition.x;
+			pos = Mathf.Abs (Mathf.FloorToInt (posX / cellWidth));
+		} else if (scrollMode == ScrollMode.Vertical) 
+		{
+			posY = panel.localPosition.y;
+			pos = Mathf.Abs (Mathf.FloorToInt (posY / cellHeight));
+		}
+
+		// Batch Update
+		for (int i = -1; i < bundle; i++)
+		{
+			int seq = i + pos;
+			AddItem (i + pos);
+		}
     }
 }
--- Assets/DietScroller/Scripts/Item.cs
+++ Assets/DietScroller/Scripts/Item.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class Item : MonoBehaviour {
     Transform tr;
     public float min = -100, max = 500f;
-
+	
     [HideInInspector]
     public bool on = false;
     Transform panel;
@@ -12,7 +12,7 @@ public class Item : MonoBehaviour {
     void Start()
     {
         tr = transform;
-        panel = transform.parent.parent;
+		panel = NGUITools.FindInParents<UIDraggablePanel> (gameObject).transform;
     }
 
     void OnOutside()
@@ -20,10 +20,15 @@ public class Item : MonoBehaviour {
         Destroy(gameObject);
     }
 
+	bool IsOutside ()
+	{
+		Vector3 pos = tr.localPosition + panel.localPosition;
+		return (pos.y > max || pos.y < min) || (pos.x > max || pos.x < min);
+	}
+
     void Update()
     {
-        Vector3 pos = tr.localPosition + panel.localPosition;
-        if (pos.x > max || pos.x < min)
+        if (IsOutside())
         {
             if (!on) OnOutside();
         }
